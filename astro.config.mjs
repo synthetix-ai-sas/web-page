38983ca6f648a40fd45879e5e60dcca55a90c53b
cristianMontes4063 <cristian.montes4063@hotmail.com>
2026-09-06 13:42:34 -0500

    feat: generate sitemap.xml via @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.synthetixaisas.com',
+
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/404'),
+    }),
+  ],
 
   adapter: vercel(),
 });
